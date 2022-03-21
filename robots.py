88b96ce5731ed52decfc89c738307b78723eafb5
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 13:02:26 -0400

    Created the instance variables for the Robots Class in robots.py

--- robots.py
+++ robots.py
@@ -1,2 +1,9 @@
+from weapon import Weapon
 class Robots:
-    pass
+    def __init__(self,name):
+        self.name = name
+        self.health = 100
+        self.weapon = Weapon("Sword", 100)
+
+    def attack(self,dinosaur):
+        pass
