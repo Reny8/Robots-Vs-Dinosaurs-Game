63fc04381408a3dcc6df754bfd0e47ec5471b0f9
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 14:31:12 -0400

    Completed the attack_dino method for the Robot class in robot.py and tested the function

--- dinosaur.py
+++ dinosaur.py
@@ -1,8 +1,11 @@
+
 class Dinosaur:
     def __init__(self,name,attack_power):
         self.name = name
         self.attack_power = attack_power
         self.health = 100
 
-    def attack(self,robot):
-        pass
+    def attack_robot(self,robot):
+        robot.health -= 10
+        print(f"{self.name} has attacked {robot.name}")
+        print(f"{robot.name} current health is {robot.health} ")
--- robot.py
+++ robot.py
@@ -1,16 +1,17 @@
 from weapon import Weapon
 from dinosaur import Dinosaur
-class Robots:
+class Robot:
     def __init__(self,name):
         self.name = name
         self.health = 100
         self.weapon = Weapon("Sword", 100)
 
-    def attack(self,dinosaur):
+    def attack_dinosaur(self,dinosaur):
         dinosaur.health -= 10
         print(f"{self.name} has attacked {dinosaur.name}")
         print(f"{dinosaur.name} current health is {dinosaur.health}")
 
-robot_1 = Robots("Roger")
-dino_1 = Dinosaur("Rex",100)
-robot_1.attack(dino_1)
+robot_1 = Robot("Bot")
+dino_1 = Dinosaur("Rex", 100)
+robot_1.attack_dinosaur(dino_1)
+robot_1.attack_dinosaur(dino_1)
