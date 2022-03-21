35a9f95d7b0b072791e16a5670f6d6370fe552a6
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 17:31:25 -0400

    Completed the create_herd method in herd.py and tested

--- herd.py
+++ herd.py
@@ -4,4 +4,10 @@ class Herd:
         self.dinosaurs = []
     
     def create_herd(self):
-        pass
+        dino_1 = Dinosaur("T-Rex", 60)
+        dino_2 = Dinosaur("Raptor", 30)
+        dino_3 = Dinosaur("Pteridactyl", 10)
+        self.dinosaurs.append(dino_1)
+        self.dinosaurs.append(dino_2)
+        self.dinosaurs.append(dino_3)
+
