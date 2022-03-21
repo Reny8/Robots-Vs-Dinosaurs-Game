6b4405db5b15fb175123705db3065bc4c21747fc
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 17:37:21 -0400

    Completed the create_fleet method for the Fleet class in fleet.py

--- fleet.py
+++ fleet.py
@@ -3,7 +3,11 @@ class Fleet:
     def __init__(self):
         self.robots = []
     
-    def create_fleet(self,robot):
-        self.robots.append(robot)  
-
+    def create_fleet(self):
+        robot_1 = Robot("Titanium")
+        robot_2 = Robot("Steel")
+        robot_3 = Robot("Chromium")
+        self.robots.append(robot_1)
+        self.robots.append(robot_2)
+        self.robots.append(robot_3)
 
