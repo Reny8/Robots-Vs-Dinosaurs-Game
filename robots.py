81fe4a4016514b268d627f5ebfce35510fab6752
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 14:16:05 -0400

    completed the attack method for robots and tested in robots.py

--- robots.py
+++ robots.py
@@ -1,4 +1,5 @@
 from weapon import Weapon
+from dinosaur import Dinosaur
 class Robots:
     def __init__(self,name):
         self.name = name
@@ -6,4 +7,10 @@ class Robots:
         self.weapon = Weapon("Sword", 100)
 
     def attack(self,dinosaur):
-        pass
+        dinosaur.health -= 10
+        print(f"{self.name} has attacked {dinosaur.name}")
+        print(f"{dinosaur.name} current health is {dinosaur.health}")
+
+robot_1 = Robots("Roger")
+dino_1 = Dinosaur("Rex",100)
+robot_1.attack(dino_1)
