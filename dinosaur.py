2ea7267f37a6b6dbd133094451c2896722dac9d8
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 13:12:02 -0400

    Created the instance variables for the Dinosaur class in dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -1,2 +1,8 @@
 class Dinosaur:
-    pass
+    def __init__(self,name,attack_power):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = 100
+
+    def attack(self,robot):
+        pass
