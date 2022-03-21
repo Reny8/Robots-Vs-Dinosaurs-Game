6cff1e6b4398cced32a90ca15a829f5fd415a477
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 13:40:55 -0400

    Created the instance variables for Battlefield class in battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -1,2 +1,30 @@
+from fleet import Fleet
+from herd import Herd
 class Battlefield:
-    pass
+    def __init__(self):
+        self.fleet = Fleet()
+        self.herd = Herd()
+    
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def dino_turn(self,dinosaur):
+        pass
+
+    def robo_turn(self,robot):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
