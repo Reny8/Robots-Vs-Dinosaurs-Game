83eed086328cf2da3fcdab10c208ca6d275c8a8d
Reny Reyes <renyreyes8@hotmail.com>
2022-03-22 16:54:22 -0400

    Edited the print statements for the choose_weapon method and added an elif to account for the weapon remaining the same

--- robot.py
+++ robot.py
@@ -11,13 +11,18 @@ class Robot:
         print(f"{dinosaur.name} current health is {dinosaur.health}")
 
     def choose_weapon(self):
-        list = [Weapon("Grenades",40), Weapon("Lethal Darts", 60)]
-        print(f"Type 0 for {list[0].name} Attack Strength: {list[0].attack_power}. ")
-        print(f"Type 1 for {list[1].name} Attack Strength: {list[1].attack_power}. ")
+        list = [Weapon("Grenades",40), Weapon("Lethal Darts", 60), Weapon("Lasers", 20)]
+        print(f"Type 0 for {list[0].name}, Attack Strength: {list[0].attack_power}. ")
+        print(f"Type 1 for {list[1].name}, Attack Strength: {list[1].attack_power}. ")
+        print(f"Type 2 to continue with the default weapon: {list[2].name}, Attack Strength: {list[2].attack_power}.")
+
         weapon_choice = int(input("Choose a weapon for your Robot: "))
         if weapon_choice == 0:
             self.weapon = Weapon("Grenades", 40)
-            print("Your new weapon is Grenades")
+            print("Your new weapon is Grenades! ")
         elif weapon_choice == 1:
             self.weapon = Weapon("Lethal Darts",60)
-            print("Your new weapon is Lethal Darts")
+            print("Cool! Your new weapon is Lethal Darts! ")
+        elif weapon_choice == 2:
+            self.weapon = Weapon("Lasers", 20)
+            print("Your weapon will remain the same. ")
