043f4f47377a5abf440e4f3fa64dcd52a66c1722
Reny Reyes <renyreyes8@hotmail.com>
2022-03-21 21:05:09 -0400

    Nested the robo_opponent method and the dino_opponent method inside of the dino_turn method and the robo_turn method

--- battlefield.py
+++ battlefield.py
@@ -15,19 +15,31 @@ class Battlefield:
     def battle(self):
         pass
 
-    def dino_turn(self,dinosaur):
-        pass
+    def dino_turn(self):
+        self.show_robo_opponent_options()
+        dino_choice = input("Choose your Dinosaur: ")
 
-    def robo_turn(self,robot):
-        pass
+        self.show_dino_opponent_options()
+        chosen_robot_to_attack = input("Choose the Robot you want to attack: ")
+
+    def robo_turn(self):
+        self.show_dino_opponent_options()
+        robo_choice = input("Choose your Robot: ")
+
+        self.show_robo_opponent_options()
+        chosen_dino_to_attack = input("Choose the Dinosaur you want to attack: ")
+
 
     def show_dino_opponent_options(self):
         self.fleet.create_fleet()
-        for robo in self.fleet.robots:
-            print(f"Robot: {robo.name}, Weapon Attack Power: {robo.weapon.attack_power}, Health: {robo.health}")
+        print(f"Type 0 for: {self.fleet.robots[0].name}, Health: {self.fleet.robots[0].health}")
+        print(f"Type 1 for: {self.fleet.robots[1].name}, Health: {self.fleet.robots[1].health}")
+        print(f"Type 2 for: {self.fleet.robots[2].name}, Health: {self.fleet.robots[2].health}")
+
     def show_robo_opponent_options(self):
         self.herd.create_herd()
-        for dino in self.herd.dinosaurs:
-            print(f"Dinosaur: {dino.name}, Attack Power: {dino.attack_power}, Health: {dino.health}")
+        print(f"Type 0 for: {self.herd.dinosaurs[0].name}, Health: {self.herd.dinosaurs[0].health}")
+        print(f"Type 1 for: {self.herd.dinosaurs[1].name}, Health: {self.herd.dinosaurs[1].health}")
+        print(f"Type 2 for: {self.herd.dinosaurs[2].name}, Health: {self.herd.dinosaurs[2].health}")
     def display_winners(self):
         pass
