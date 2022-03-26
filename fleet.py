940446b37f4af6dbf8660bc4cd9dd97ba20c263f
Reny Reyes <renyreyes8@hotmail.com>
2022-03-25 22:32:12 -0400

    Created a counter for loop for the list of dinosaurs and the list of robots

--- fleet.py
+++ fleet.py
@@ -4,12 +4,7 @@ class Fleet:
         self.robots = [Robot("Titanium"), Robot("Steel"), Robot("Chromium")]
     
     def create_fleet(self):
-        if len(self.robots) == 3:
-            print(f"Type 0 for: {self.robots[0].name}, Health: {self.robots[0].health}")
-            print(f"Type 1 for: {self.robots[1].name}, Health: {self.robots[1].health}")
-            print(f"Type 2 for: {self.robots[2].name}, Health: {self.robots[2].health}")
-        elif len(self.robots) == 2:
-            print(f"Type 0 for: {self.robots[0].name}, Health: {self.robots[0].health}")
-            print(f"Type 1 for: {self.robots[1].name}, Health: {self.robots[1].health}")
-        elif len(self.robots) == 1:
-            print(f"Type 0 for: {self.robots[0].name}, Health: {self.robots[0].health}")
+        counter = 0
+        for robot in self.robots:
+            print(f"Type {counter} for: {robot.name}, Health: {robot.health}")
+            counter += 1
--- herd.py
+++ herd.py
@@ -4,14 +4,9 @@ class Herd:
         self.dinosaurs = [Dinosaur("T-Rex", 60),Dinosaur("Raptor", 30), Dinosaur("Pteridactyl", 10)]
     
     def create_herd(self):
-        if len(self.dinosaurs) == 3:
-            print(f"Type 0 for: {self.dinosaurs[0].name}, Health: {self.dinosaurs[0].health}")
-            print(f"Type 1 for: {self.dinosaurs[1].name}, Health: {self.dinosaurs[1].health}")
-            print(f"Type 2 for: {self.dinosaurs[2].name}, Health: {self.dinosaurs[2].health}")
-        elif len(self.dinosaurs) == 2:
-            print(f"Type 0 for: {self.dinosaurs[0].name}, Health: {self.dinosaurs[0].health}")
-            print(f"Type 1 for: {self.dinosaurs[1].name}, Health: {self.dinosaurs[1].health}")
-        elif len(self.dinosaurs) == 1:
-            print(f"Type 0 for: {self.dinosaurs[0].name}, Health: {self.dinosaurs[0].health}")
+        counter = 0
+        for dinosaur in self.dinosaurs:
+            print(f"Type {counter} for: {dinosaur.name}, Health: {dinosaur.health}")
+            counter += 1
 
         
